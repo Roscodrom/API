dfb97a73f87e8bdcba988d14b00d040911c3da7f
Threx171 <alejandro_1173@hotmail.com>
2024-04-19 18:18:59 +0200

    added script to process files into db, added endpoint to get words by pages from db

---
+++ data/DISC2-LP/DISC2/readme.txt
@@ -0,0 +1,19 @@
+Diccionari Informatitzat de l'Scrabble en Català, versió 2.14.21 - 11/09/2022
+-----------------------------------------------------------------------------
+
+Copyright 2012 -- 2023 Joan Montané joan(sense_spam)montane.cat
+
+El «Diccionari Informatitzat de l'Scrabble en Català», DISC, té una llicència dual GPLv3 i CC-by-sa v3, a la vostra elecció
+
+Per a més informació, visiteu:
+
+http://diccionari.totescrable.cat
+
+
+«Diccionari Informatitzat de l'Scrabble en Català» (DISC) is under a dual license (at your choose): GPLv3 and CC-by-sa v3
+
+For more info, read (in Catalan):
+
+http://diccionari.totescrable.cat
+
+
--- src/app.js
+++ src/app.js
@@ -1,16 +1,49 @@
 const express = require('express');
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
 const mongoose = require('mongoose');
 const dbConfig = require('./config/db');
 const userRoutes = require('./api/routes/userRoutes');
 const Event = require('./api/models/event');
 const app = express();
+const port = process.env.PORT || 3000;
+
+const Schema = mongoose.Schema;
+const directoryPath = path.join(__dirname, '../data/dicts');
 
 app.use(express.json());
 app.set('json spaces', 2);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
 
-mongoose.connect(dbConfig.MONGODB_URI).then(() => console.log("Connectat a MongoDB"))
-  .catch(err => console.error("No s'ha pogut connectar a MongoDB", err));
+const dictionarySchema = new Schema({
+  word: String,
+  language: String,
+  times_used: Number
+});
 
+const Dictionary = mongoose.model('Dictionary', dictionarySchema);
+
+mongoose.connect(dbConfig.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    const collectionName = Dictionary.collection.collectionName;
+    return mongoose.connection.db.listCollections({name: collectionName}).next();
+  })
+  .then(collection => {
+    if (!collection) {
+      console.log("Collection does not exist. Inserting words...");
+      insertWords();
+    } else {
+      console.log("Collection already exists.");
+    }
+  })
+  .catch(err => console.error("Could not connect to MongoDB", err));
+
+  
 app.get('/api/health', (req, res) => {
   res.json({ status: "OK" });
 });
@@ -39,4 +72,57 @@ app.get('/api/events/:id', async (req, res) => {
   }
 });
 
+app.get('/api/words/:language', async (req, res) => {
+  try {
+    const language = req.params.language;
+    const page = parseInt(req.query.page) || 1;
+    const limit = 20;
+    const skip = (page - 1) * limit;
+
+    const words = await Dictionary.find({ language: language })
+      .skip(skip)
+      .limit(limit);
+
+    res.send(words);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+
+
 module.exports = app;
+
+async function processFile(filePath, language) {
+  const fileStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  let words = [];
+  for await (const line of rl) {
+    words.push({word: line, language: language, times_used: 0});
+
+    if (words.length >= 500) {
+      await Dictionary.insertMany(words);
+      words = [];
+    }
+  }
+
+  if (words.length > 0) {
+    await Dictionary.insertMany(words);
+  }
+}
+
+function insertWords() {
+  const files = fs.readdirSync(directoryPath);
+
+  for (let i = 0; i < files.length; i++) {
+    const filePath = path.join(directoryPath, files[i]);
+    const language = files[i].split('_')[0];
+    processFile(filePath, language)
+      .then(() => console.log(`File processed: ${files[i]}`))
+      .catch(err => console.error(err));
+  }
+}
