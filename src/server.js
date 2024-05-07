6af7f36ec5c2e7968e20537cb94610edcb2a8307
MatillaSan <118919845+MatillaSan@users.noreply.github.com>
2024-05-07 17:12:06 +0200

    comit

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const mongoose = require('mongoose');
 const Partida = require('./api/models/partida');
 const Dictionary = require('./api/models/dictionary');
+const Action = require('./api/models/action');
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
@@ -18,6 +19,8 @@ class Joc {
     this.playersInQueue = [];
     this.roscoLetters = [];
     this.gameInfo = {};
+    this.countA = 0;
+    this.countB = 0;
     this.partidaDuracio = partidaDuracio;
     this.pausaDuracio = pausaDuracio;
     this.properInici = Date.now() + this.pausaDuracio;
@@ -25,7 +28,8 @@ class Joc {
     this.iniciarCicle();
   }
 
-  iniciarCicle() {
+  async iniciarCicle() {
+    this.roscoLetters = this.generateRoscoLetters();
     setInterval(() => {
       if (this.playersInQueue != null && this.playersInQueue.length >= 1 && !this.enPartida) {
         console.log("Iniciando partida");
@@ -50,7 +54,6 @@ class Joc {
 
   async startGame() {
     const idCount = await Partida.countDocuments().exec();
-    this.roscoLetters = this.generateRosco();
     this.gameInfo = {
       "tipus": "multijugador",
       "id_partida": idCount + 1,
@@ -69,20 +72,24 @@ class Joc {
     }
     console.log("Enviando informacion a los espectadores");
     for (let client of this.flutterConnections) {
-      client.socket.send(JSON.stringify({ type: 'GAME_INFO', data: { players_list: this.playersInGame } }));
+      client.send(JSON.stringify({ type: 'GAME_INFO', data: { enPartida: joc.enPartida, message: joc.cleanPlayersList() } }));
     }
     this.playersInQueue.splice(0, this.playersInQueue.length);
     console.log("Añadiendo partida a la base de datos");
     const newPartida = new Partida(this.gameInfo);
-    newPartida.save();
+    await newPartida.save();
   }
 
-  endGame() {
+  async endGame() {
     console.log("Eliminando a los jugadores de la partida");
     for (let player of this.playersInGame) {
       this.gameInfo.puntuacio_usuaris[player.nickname] = player.points;
       player.socket.send(JSON.stringify({ type: 'GAME_FINISHED', data: { points: player.points } }));
       player.socket.terminate();
+    }
+
+    for (let client of this.flutterConnections) {
+      client.send(JSON.stringify({ type: 'GAME_FINISHED', data: { enPartida: false, message: joc.cleanPlayersList() } }));
     }
 
     console.log("Actualizando datos de partida");
@@ -97,16 +104,10 @@ class Joc {
       }
     };
 
-    Partida.updateOne(
+    await Partida.updateOne(
       { id_partida: this.gameInfo.id_partida },
       nuevosDatos
     )
-  }
-
-  generateRosco() {
-    console.log("Generando rosco");
-    let lettersList = ["E", "A", "O", "I", "N", "Y", "R", "N", "D", "G", "Q"];
-    return lettersList;
   }
 
   async checkUserWord(player_word) {
@@ -135,12 +136,80 @@ class Joc {
       return 0;
     }
   }
+
+  generateRosco() {
+    console.log("Generando rosco");
+    let lettersList = ["E", "A", "O", "I", "N", "Y", "R", "N", "D", "G", "Q"];
+    return lettersList;
+  }
+
+  generateRoscoLetters() {
+    let rosco_letters = [];
+    let countVocales = 0;
+
+    for (let i = 0; i < 10; i++) {
+      let isVocal = false;
+      if (i < 3) { // Las tres primeras letras son vocales
+        isVocal = true;
+        countVocales++;
+      } else if (i < 5) { // Aqui se calcula si hay vocales extra o consonantes extra
+        if (countVocales == 3 && Math.random() < 0.5) {
+          isVocal = true;
+          countVocales++;
+        } else if (countVocales == 4 && Math.random() < 0.5) {
+          isVocal = true;
+          countVocales++;
+        }
+      }
+      rosco_letters[i] = this.getRandomLetter(isVocal, rosco_letters);
+    }
+    return rosco_letters;
+  }
+
+  getRandomLetter(isVocal, rosco_letters) {
+    let newLetter = "";
+    let letterInRosco = true;
+    let badLetter = true;
+    let vocales = ["A", "E", "I", "O", "U"];
+    let consonantes = [
+      "B", "C", "D", "F", "G", "H", "J", "K", "L", "M",
+      "N", "NY", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"];
+
+    while (letterInRosco) {
+      newLetter = isVocal ? vocales[Math.floor(Math.random() * vocales.length)]
+        : consonantes[Math.floor(Math.random() * consonantes.length)];
+      letterInRosco = this.checkLettersInRosco(newLetter, rosco_letters);
+    }
+
+    return newLetter;
+  }
+
+  checkLettersInRosco(newLetter, rosco_letters) {
+    for (let letter of rosco_letters) {
+      if (letter == newLetter) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  cleanPlayersList() {
+    let listaDeJugadores = []
+    for (let player of this.playersInGame) {
+      listaDeJugadores.push({
+        nickname: player.nickname,
+        points: player.points
+      });
+    }
+    return listaDeJugadores;
+  }
 }
 
 const joc = new Joc(60000, 60000);  // 1 minut de partida, 1 minut de pausa
 
 wss.on('connection', (socket) => {
   console.log('Usuario conectado');
+  const client_id = generateClientId();
   socket.send(JSON.stringify({ type: 'HANDSHAKE', data: { connection: 201 } }));
 
   const intervalId = setInterval(() => {
@@ -160,7 +229,7 @@ wss.on('connection', (socket) => {
         console.log('Flutter');
         joc.flutterConnections.push(socket);
         if (joc.enPartida) {
-          socket.send(JSON.stringify({ type: 'WELLCUM', data: { enPartida: true, message: joc.playersInGame } }));
+          socket.send(JSON.stringify({ type: 'GAME_INFO', data: { enPartida: joc.enPartida, message: joc.cleanPlayersList() } }));
         } else {
           socket.send(JSON.stringify({ type: 'WELLCUM', data: { enPartida: false, message: "Alex guapo!" } }));
         }
@@ -170,6 +239,7 @@ wss.on('connection', (socket) => {
           id: data.data.id,
           socket: socket,
           nickname: data.data.nickname,
+          client_id: client_id,
           points: 0
         });
         socket.send(JSON.stringify({ type: 'WELLCUM', data: { connection: 201, message: "Añadido a la lista de espera!" } }));
@@ -185,16 +255,36 @@ wss.on('connection', (socket) => {
     else if (data.type == 'CHECK_WORD') {
       console.log("Se ha recivido la palabra: " + data.data.word)
       const word_points = await joc.checkUserWord(data.data.word);
-      console.log(word_points);
       socket.send(JSON.stringify({ type: 'WORD_POINTS', data: { word: data.data.word, points: word_points } }));
+
       if (word_points != 0) {
+        console.log(word_points);
+        joc.gameInfo['paraules_puntuades'].push(data.data.word);
         for (player of joc.playersInGame) {
-          player.socket.send(JSON.stringify({ type: 'NEW_WORD', data: { word: data.data.word, points: word_points } }));
+          if (player.client_id == client_id) {
+            player.points += word_points;
+          } else {
+            player.socket.send(JSON.stringify({ type: 'NEW_WORD', data: { word: data.data.word, points: word_points } }));
+          }
         }
         for (client of joc.flutterConnections) {
-          client.socket.send(JSON.stringify({ type: 'NEW_WORD', data: { word: data.data.word, points: word_points } }));
+          client.send(JSON.stringify({ type: 'NEW_WORD', data: { word: data.data.word, points: word_points } }));
+          client.send(JSON.stringify({ type: 'GAME_INFO', data: { enPartida: joc.enPartida, message: joc.cleanPlayersList() } }));
         }
       }
+
+      // Guardar la accion en mongo
+      const newAction = new Action({
+        id_partida: joc.gameInfo.id_partida,
+        tipus_accio: "paraula",
+        uuid_jugador: "",
+        data: Date.now(),
+        dades_adicionals: {
+          paraula_enviada: data.data.word,
+          puntuacio_obtinguda: word_points
+        }
+      });
+      await newAction.save();
     }
 
     else {
@@ -207,3 +297,7 @@ wss.on('connection', (socket) => {
     clearInterval(intervalId);  // Detiene el envío periódico cuando el usuario se desconecta
   });
 });
+
+function generateClientId() {
+  return Math.random().toString(36).substring(7);
+}
